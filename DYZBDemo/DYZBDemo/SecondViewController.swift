576a025051c7951bfa7413b890bb70d4e88a4d62
admin <740264559@qq.com>
2017-12-13 23:00:27 +0800

    初始化项目

---
+++ DYZBDemo/DYZBDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DYZBDemo
+//
+//  Created by admin on 17/12/13.
+//  Copyright © 2017年 tdin360. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
